91002b0d41990a652b9a0fbf14c96eeeca292647
Garsantsyan <garsantsyan1@mail.ru>
2021-10-05 16:43:58 +0300

    Initial commit

---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,30 @@
+package chapter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if(x < y) {
+            System.out.println("x меньше y");
+        }
+
+        x = x * 2;
+
+        if(x == y) {
+            System.out.println("x теперь равно y");
+        }
+
+        x = x * 2;
+
+        if(x > y) {
+            System.out.println("x теперь больше y");
+        }
+
+        if(x == y) {
+            System.out.println("вы не увидите этого");
+        }
+    }
+}
