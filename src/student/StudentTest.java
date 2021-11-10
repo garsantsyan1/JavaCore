c712bb93c340bf79e137164cd9f1b2d4312077c6
Garsantsyan <garsantsyan1@mail.ru>
2021-11-10 16:52:45 +0300

    add student, braceChecker

---
+++ src/student/StudentTest.java
@@ -0,0 +1,10 @@
+package student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student("Poxos", "Poxosyan", 23,
+                "098778877", "java");
+
+        System.out.println(student.getName());
+    }
+}
